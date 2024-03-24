33f4aa5b50ff29a8494126ae73a0bc44491a5e30
K1r1X <timmayatsky@gmail.com>
2024-03-24 20:02:06 +0300

    I modified my program!

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 
-const std::string VERSION{ "1.0.0" };
+const std::string VERSION{ "1.0.1" };
+const std::string AUTHOR{ "K1r1X" };
 
 int main(void)
 {
-	std::cout << "It's my program!\nVersion: " << VERSION << std::endl;
+	std::cout << "It's my program!\nI modified it and added author's name!\nVersion: " << VERSION
+		 << std::endl << "Author: " << AUTHOR << std::endl;
 
 	return EXIT_SUCCESS;
 }
